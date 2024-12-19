d7842d9e8023d42ad0458e21185548725c646055
polyakale <polyakalex52@gmail.com>
2024-12-19 12:21:33 +0100

    registration fail

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -24,8 +24,8 @@ Route::get('users', [UsersController::class, 'index'])
 
 Route::get('users/{id}', [UsersController::class, 'show'])
     ->middleware('auth:sanctum');
-Route::post('users', [UsersController::class, 'store'])
-    ->middleware('auth:sanctum');    
+Route::post('users', [UsersController::class, 'store']);
+    // ->middleware('auth:sanctum');    
 Route::patch('users/{id}', [UsersController::class, 'update'])
     ->middleware('auth:sanctum');    
 Route::delete('users/{id}', [UsersController::class, 'destroy'])
