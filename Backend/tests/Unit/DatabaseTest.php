d409f256baa8c7e530fc9329bcdcace8de54da18
polyakale <polyakalex52@gmail.com>
2025-01-07 13:09:40 +0100

    changess

---
+++ Backend/tests/Unit/DatabaseTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_database_creation_and_tables_exists(): void
+    {
+        $this->assertTrue(true);
+    }
+}
--- Backend/tests/Unit/ExampleTest.php
+++ Backend/tests/Unit/ExampleTest.php
@@ -2,13 +2,7 @@
 
 namespace Tests\Unit;
 
-use App\Models\User;
-use Illuminate\Container\Attributes\DB as AttributesDB;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
-// use PHPUnit\Framework\TestCase;
-use Tests\TestCase;
+use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
@@ -16,17 +10,15 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
-    {
-        $valami = true;
-        // dd($valami);
-        $this->assertTrue($valami);
-    }
+    // public function test_that_true_is_true(): void
+    // {
+    //     $valami = true;
+    //     // dd($valami);
+    //     $this->assertTrue($valami);
+    // }
 
-    public function sajat_test(): void{
-        $valami = 2;
-        $this->assertGreaterThan(1,$valami);
-    }
-
-    
+    // public function test_sajat_test(): void{
+    //     $valami = 2;
+    //     $this->assertGreaterThan(1,$valami);
+    // }
 }
