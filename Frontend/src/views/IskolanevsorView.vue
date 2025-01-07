263b433076d1005bcee8101c4baf325284bc2ea7
polyakale <polyakalex52@gmail.com>
2025-01-07 12:03:11 +0100

    x

--- Frontend/src/views/IskolanevsorView.vue
+++ Frontend/src/views/IskolanevsorView.vue
@@ -3,9 +3,9 @@
     <h1>Iskolanévsor</h1>
     <div v-for="(oszt, index) in osztalynevsorok" :key="index">
       <h3>{{ oszt.osztalyNev }}</h3>
-        <ul v-for="nev in oszt.nevek" :key="nev">
-            <li>{{ nev }}</li>
-        </ul>
+      <ul v-for="nev in oszt.nevek" :key="nev">
+        <li>{{ nev }}</li>
+      </ul>
     </div>
   </div>
 </template>
