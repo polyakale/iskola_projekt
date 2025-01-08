6e85df25d2bd550cf24478c765c14f132f7e7483
polyakale <polyakalex52@gmail.com>
2025-01-08 14:06:06 +0100

    test changes

--- Backend/tests/Feature/SportsTest.php
+++ Backend/tests/Feature/SportsTest.php
@@ -3,15 +3,25 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Sport;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class SportsTest extends TestCase
 {
+    use DatabaseTransactions;
+
     public function test_the_get_sports_table_all_record_example(): void
     {
-        
-        $response = $this->get('/');
-
+        //Bera...
+        $row = Sport::create([
+            'sportNev' => 'xxx'
+        ]);
+        $response = $this->get('/api/sports');
         $response->assertStatus(200);
+        $response->assertSee('xxx');
+
+        $this->assertDatabaseHas('sports', ['sportNev' => 'xxx']);
     }
 }
