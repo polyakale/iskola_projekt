2d619047c5555ab36c07cd0906fc6663ba41d90b
polyakale <polyakalex52@gmail.com>
2025-01-23 11:26:45 +0100

    not good

---
+++ Frontend/src/components/OsztalyForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <p>{{ dataLine }}</p>
+    <form class="row g-4 needs-validation was-validated" novalidate>
+      <!-- Name -->
+      <div class="col-md-8">
+        <label for="name" class="form-label">Class Name:</label>
+        <input
+          type="text"
+          class="form-control"
+          id="name"
+          required
+          v-model="dataLine.osztalyNev"
+        />
+        <div class="valid-feedback">&nbsp;</div>
+        <div class="invalid-feedback">
+          Class name is required to be filled out
+        </div>
+      </div>
+
+      <button type="submit" class="btn btn-success">Save</button>
+    </form>
+  </div>
+</template>
+    
+    <script>
+export default {
+  props: ["dataLine"],
+  emits: ["saveItem"],
+  mounted() {
+    const forms = document.querySelectorAll(".needs-validation");
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach((form) => {
+      form.addEventListener(
+        "submit",
+        (event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          if (form.checkValidity()) {
+            this.onClickSubmit();
+          }
+        },
+        false
+      );
+    });
+  },
+  methods: {
+    onClickSubmit() {
+      console.log("Save");
+      this.$emit("saveItem", this.dataLine);
+    },
+  },
+};
+</script>
+    
+    <style>
+</style>
--- Frontend/src/views/Osztalyok.vue
+++ Frontend/src/views/Osztalyok.vue
@@ -1,13 +1,206 @@
 <template>
+  <div>
     <h1>Osztályok</h1>
-  </template>
-  
-  <script>
-  export default {
-  
+    <!-- Table -->
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <!-- Módositás -->
+          <th scope="col">Operations</th>
+          <th scope="col">OsztalyNev</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in collection"
+          :key="item.id"
+          @click="onClickTr(item.id)"
+          :class="{ 'table-success': selectedId == item.id }"
+        >
+          <td class="text-nowrap">
+            <OperationsCrud
+              :item="item"
+              @onClickDeleteButton="onClickDeleteButton"
+              @onClickUpdate="onClickUpdate"
+              @onClickCreate="onClickCreate"
+            />
+          </td>
+          <!-- Módositás -->
+          <td>{{ item.osztalyNev }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- Modal -->
+    <Modal
+      :title="title"
+      :yes="yes"
+      :no="no"
+      :size="size"
+      @yesEvent="yesEventHandler"
+    >
+      <!-- yes-no (Modal) -->
+      <div v-if="state == 'Delete'">
+        {{ messageYesNo }}
+      </div>
+      <!-- Form sport -->
+      <DataForm
+        v-if="state == 'Create' || state == 'Update'"
+        :dataLine="dataLine"
+        @saveItem="saveItemsHandler"
+      />
+    </Modal>
+  </div>
+</template>
+
+<script>
+import { BASE_URL } from "../helpers/baseUrls";
+import { useAuthStore } from "@/stores/useAuthStore";
+import OperationsCrud from "@/components/OperationsCrud.vue";
+// Móddositás
+import DataForm from "@/components/OsztalyForm.vue";
+import Modal from "@/components/Modal.vue";
+import * as bootstrap from "bootstrap";
+import axios from "axios";
+
+// Módositás
+class DataLine {
+  constructor(osztalyNev = null) {
+    this.osztalyNev = osztalyNev;
   }
-  </script>
-  
-  <style>
-  
-  </style>
+}
+
+export default {
+  components: { OperationsCrud, Modal, DataForm },
+  data() {
+    return {
+      modal: null,
+      selectedId: null,
+      messageYesNo: null,
+      state: "Read",
+      title: null,
+      yes: null,
+      no: null,
+      size: null,
+      collection: [],
+      store: useAuthStore(),
+      dataLine: new DataLine(),
+      // Módositás
+      urlBase: `${BASE_URL}/osztalies`,
+    };
+  },
+  mounted() {
+    this.getCollections();
+    this.modal = new bootstrap.Modal("#modal", {
+      keyboard: false,
+    });
+  },
+  methods: {
+    async getCollections() {
+      const url = this.urlBase;
+      const response = await axios.get(url);
+      this.collection = response.data.data;
+      console.log(this.collection);
+    },
+    async deleteDataLine() {
+      const url = `${this.urlBase}/${this.selectedId}`;
+      const token = this.store.token;
+      const headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      };
+      console.log("headers, url:", headers, url);
+
+      try {
+        await axios.delete(url, {
+          headers: headers,
+        });
+        this.getCollections();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async createDataLine() {
+      const url = this.urlBase;
+      const token = this.store.token;
+      const headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      };
+      console.log("dataline", this.dataLine);
+      console.log("headers", headers);
+      try {
+        await axios.post(url, this.dataLine, {
+          headers: headers,
+        });
+        this.getCollections();
+      } catch (error) {
+        console.log(error.response.status);
+      }
+    },
+    async updateDataLine() {
+      const url = `${this.urlBase}/${this.selectedId}`;
+      const token = this.store.token;
+      const headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      };
+      console.log("dataline", this.dataLine);
+      console.log("headers", headers);
+      try {
+        await axios.patch(url, this.dataLine, {
+          headers: headers,
+        });
+        this.getCollections();
+      } catch (error) {
+        console.log(error.response.status);
+      }
+    },
+    yesEventHandler() {
+      if (this.state === "Delete") {
+        this.deleteDataLine();
+        this.modal.hide();
+      }
+    },
+    onClickDeleteButton(dataLine) {
+      this.title = "Delete";
+      this.messageYesNo = `Are you sure you want to delete it? ${dataLine.osztalyNev}`;
+      this.yes = "Yes";
+      this.no = "No";
+      this.size = null;
+      this.state = "Delete";
+      this.selectedId = dataLine.id;
+    },
+    onClickUpdate(dataLine) {
+      this.state = "Update";
+      this.title = "Modify";
+      this.yes = null;
+      this.no = "Cancel";
+      this.size = "lg";
+      this.dataLine = dataLine;
+    },
+    onClickCreate() {
+      this.title = "New create";
+      this.yes = null;
+      this.no = "Cancel";
+      this.size = "lg";
+      this.state = "Create";
+      this.dataLine = new DataLine();
+    },
+    onClickTr(id) {
+      this.selectedId = id;
+    },
+    saveItemsHandler(dataLine) {
+      this.dataLine = dataLine;
+      this.modal.hide();
+      if (this.state == "Create") {
+        this.createDataLine();
+      } else if (this.state == "Update") {
+        this.updateDataLine();
+      }
+    },
+  },
+};
+</script>
